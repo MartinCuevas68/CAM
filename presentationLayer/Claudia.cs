aa7d7f0e21f3ec877f448a6fa4dcfd7701f44e76
ClaudiaArias01 <78814719+ClaudiaArias01@users.noreply.github.com>
2021-11-05 03:35:47 -0700

    Cambios_Font

--- presentationLayer/Claudia.cs
+++ presentationLayer/Claudia.cs
@@ -9,157 +9,192 @@ namespace presentationLayer
         public static void altasButton(Button button) {
             button.Location = new Point(1100,1100);
             button.Size = new Size(100,30);
+            button.Font = new Font("Leelawadee UI", 12);
         }
 
         public static int altasLabel(Label label, int distancia) { 
-            label.Location = new Point(60, distancia);
+            
+            label.Location = new Point(100, distancia);
             label.Size = new Size(100, 30);
-            distancia = distancia + 30;
-            return distancia;
-        }
-
-        public static int altasLabelDerecha(Label label, int distancia)
-        {
-            label.Location = new Point(660, distancia);
-            label.Size = new Size(100, 30);
-            distancia = distancia + 30;
-            return distancia;
-        }
-
-        public static int altasGroupBox(GroupBox groupBox, int distancia)
-        {
-            groupBox.Location = new Point(660, distancia);
-            groupBox.Size = new Size(220, 55);
-            distancia = distancia + 260;
-            return distancia;
-        }
-
-        public static int altasDateTimePicker(DateTimePicker dateTimePicker, int distancia)
-        {
-            dateTimePicker.Location = new Point(220, distancia);
-            dateTimePicker.Size = new Size(300, 30);
-            distancia = distancia + 30;
+            label.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 40;
             return distancia;
         }
 
         public static int altasTextBox(TextBox textBox, int distancia)
         {
-            textBox.Location = new Point(220, distancia);
+            textBox.Location = new Point(310, distancia);
             textBox.Size = new Size(300, 30);
-            distancia = distancia + 30;
+            textBox.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 38;
+            return distancia;
+        }
+
+        public static int altasLabelDerecha(Label label, int distancia)
+        {
+            label.Location = new Point(980, distancia);
+            label.Size = new Size(100, 30);
+            label.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 40;
             return distancia;
         }
 
         public static int altasTextBoxDerecha(TextBox textBox, int distancia)
         {
-            textBox.Location = new Point(775, distancia);
+            textBox.Location = new Point(1130, distancia);
             textBox.Size = new Size(120, 30);
-            distancia = distancia + 30;
+            textBox.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 40;
+            return distancia;
+        }
+
+        public static int altasGroupBox(GroupBox groupBox, int distancia)
+        {
+            groupBox.Location = new Point(980, distancia);
+            groupBox.Size = new Size(280, 55);
+            groupBox.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 310;
+            return distancia;
+        }
+
+        public static int altasDateTimePicker(DateTimePicker dateTimePicker, int distancia)
+        {
+            dateTimePicker.Location = new Point(310, distancia);
+            dateTimePicker.Size = new Size(300, 30);
+            dateTimePicker.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 52;
             return distancia;
         }
 
         public static int altasTelefonosLabel(Label casa, Label celular, Label trabajo, int distancia)
         {
-            casa.Location = new Point(215, distancia + 4);
-            celular.Location = new Point(355, distancia + 4);
-            trabajo.Location = new Point(515, distancia + 4);
+            distancia = distancia + 10;
+            casa.Location = new Point(310, distancia );
+            celular.Location = new Point(510, distancia);
+            trabajo.Location = new Point(715, distancia);
             casa.Size = new Size(120, 30);
             celular.Size = new Size(120, 30);
             trabajo.Size = new Size(120, 30);
-            distancia = distancia + 30;
+            casa.Font = new Font("Leelawadee UI", 12);
+            celular.Font = new Font("Leelawadee UI", 12);
+            trabajo.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 20;
             return distancia;
         }
 
         public static void altasTelefonosTextBox(TextBox casa, TextBox celular, TextBox trabajo, int distancia)
         {
-            casa.Location = new Point(255, distancia);
-            celular.Location = new Point(415, distancia);
-            trabajo.Location = new Point(575, distancia);
+            distancia = distancia + 8;
+            casa.Location = new Point(380, distancia);
+            celular.Location = new Point(590, distancia);
+            trabajo.Location = new Point(790, distancia);
             casa.Size = new Size(90, 30);
             celular.Size = new Size(90, 30);
             trabajo.Size = new Size(90, 30);
+            casa.Font = new Font("Leelawadee UI", 12);
+            celular.Font = new Font("Leelawadee UI", 12);
+            trabajo.Font = new Font("Leelawadee UI", 12);
         }
 
         public static int altasResponsableAlumnoCheckBox(CheckBox madre, CheckBox padre, int distancia)
         {
-            madre.Location = new Point(365, distancia + 4);
-            padre.Location = new Point(480, distancia + 4);
+            madre.Location = new Point(500, distancia + 7);
+            padre.Location = new Point(670, distancia + 7);
+            madre.Font = new Font("Leelawadee UI", 12);
+            padre.Font = new Font("Leelawadee UI", 12);
             distancia = distancia + 30;
-
             return distancia;
         }
 
         public static void altasApellidosLabel(Label apellidoP, Label apellidoM , int distancia)
         {
             distancia = distancia - 30;
-            apellidoP.Location = new Point(400, distancia);
-            apellidoM.Location = new Point(660, distancia);
+            apellidoP.Location = new Point(510, distancia - 10);
+            apellidoM.Location = new Point(800, distancia - 10);
             apellidoP.Size = new Size(120, 30);
             apellidoM.Size = new Size(120, 30);
+            apellidoM.Font = new Font("Leelawadee UI", 12);
+            apellidoP.Font = new Font("Leelawadee UI", 12);
             distancia = distancia + 30;
+        }
+        
+        public static int altasApellidosTextBox(TextBox nombre, TextBox apellidoP, TextBox apellidoM, int distancia)
+        {
+            nombre.Location = new Point(310, distancia - 10);
+            apellidoP.Location = new Point(670, distancia - 10);
+            apellidoM.Location = new Point(965, distancia - 10);
+            apellidoP.Size = new Size(120, 30);
+            apellidoM.Size = new Size(120, 30);
+            nombre.Size = new Size(190, 30);
+            apellidoP.Font = new Font("Leelawadee UI", 12);
+            apellidoM.Font = new Font("Leelawadee UI", 12);
+            nombre.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 30;
+            return distancia;
         }
 
         public static void altasNacimientoAlumnoLabel(Label ciudad, Label estado, int distancia)
         {
             distancia = distancia - 30;
-            ciudad.Location = new Point(220, distancia);
-            estado.Location = new Point(400, distancia);
+            ciudad.Location = new Point(310, distancia - 8);
+            estado.Location = new Point(510, distancia - 8);
             ciudad.Size = new Size(120, 30);
             estado.Size = new Size(120, 30);
+            ciudad.Font = new Font("Leelawadee UI", 12);
+            estado.Font = new Font("Leelawadee UI", 12);
             distancia = distancia + 30;
+        }
+        
+        public static void altasNacimientoAlumnoTextBox(TextBox ciudad, TextBox estado, int distancia)
+        {
+            distancia = distancia - 34;
+            ciudad.Location = new Point(380, distancia - 6);
+            estado.Location = new Point(590, distancia - 6);
+            ciudad.Size = new Size(120, 30);
+            estado.Size = new Size(120, 30);
+            ciudad.Font = new Font("Leelawadee UI", 12);
+            estado.Font = new Font("Leelawadee UI", 12);
+            distancia = distancia + 34;
         }
 
         public static void altasDireccionLabel(Label calle, Label numero, Label colonia, int distancia)
         {
             distancia = distancia - 30;
-            calle.Location = new Point(220, distancia);
-            numero.Location = new Point(400, distancia);
-            colonia.Location = new Point(540, distancia);
+            calle.Location = new Point(310, distancia - 8);
+            numero.Location = new Point(510, distancia - 8);
+            colonia.Location = new Point(660, distancia - 8);
             calle.Size = new Size(120, 30);
             numero.Size = new Size(120, 30);
             colonia.Size = new Size(120, 30);
+            calle.Font = new Font("Leelawadee UI", 12);
+            numero.Font = new Font("Leelawadee UI", 12);
+            colonia.Font = new Font("Leelawadee UI", 12);
             distancia = distancia + 30;
         }
 
         public static int altasDireccionTextBox(TextBox calle, TextBox numero, TextBox colonia, int distancia)
         {
             distancia = distancia - 30;
-            calle.Location = new Point(270, distancia - 4);
-            numero.Location = new Point(460, distancia - 4);
-            colonia.Location = new Point(600, distancia - 4);
+            calle.Location = new Point(380, distancia - 10);
+            numero.Location = new Point(590, distancia - 10);
+            colonia.Location = new Point(740, distancia - 10);
             calle.Size = new Size(120, 30);
             numero.Size = new Size(60, 30);
             colonia.Size = new Size(120, 30);
+            calle.Font = new Font("Leelawadee UI", 12);
+            numero.Font = new Font("Leelawadee UI", 12);
+            colonia.Font = new Font("Leelawadee UI", 12);
             distancia = distancia + 30;
             return distancia;
         }
 
-        public static int altasApellidosTextBox(TextBox nombre, TextBox apellidoP, TextBox apellidoM, int distancia)
-        { 
-            nombre.Location = new Point(220, distancia);
-            apellidoP.Location = new Point(520, distancia);
-            apellidoM.Location = new Point(780, distancia);
-            apellidoP.Size = new Size(120, 30);
-            apellidoM.Size = new Size(120, 30);
-            nombre.Size = new Size(170, 30);
-            distancia = distancia + 30;
-            return distancia;
-        }
 
-        public static void altasNacimientoAlumnoTextBox(TextBox ciudad, TextBox estado, int distancia)
-        {
-            distancia = distancia - 34;
-            ciudad.Location = new Point(270, distancia);
-            estado.Location = new Point(460, distancia);
-            ciudad.Size = new Size(120, 30);
-            estado.Size = new Size(120, 30);
-            distancia = distancia + 34;
-        }
 
         public static int altasResponsablesOtroAlumnoGroupBox(GroupBox otroAlumno, int distancia)
         {
             otroAlumno.Location = new Point(60, distancia + 4);
             otroAlumno.Size = new Size(410, 50);
+            otroAlumno.Font = new Font("Leelawadee UI", 12);
             distancia = distancia + 30;
 
             return distancia;
@@ -185,6 +220,17 @@ namespace presentationLayer
             numeroTB.Size = new Size(60, 30);
             colonia.Size = new Size(120, 30);
             coloniaTB.Size = new Size(120, 30);
+
+            ocupacion.Font = new Font("Leelawadee UI", 12);
+            lugar.Font = new Font("Leelawadee UI", 12);
+            calle.Font = new Font("Leelawadee UI", 12);
+            calleTB.Font = new Font("Leelawadee UI", 12);
+            numero.Font = new Font("Leelawadee UI", 12);
+            numeroTB.Font = new Font("Leelawadee UI", 12);
+            colonia.Font = new Font("Leelawadee UI", 12);
+            coloniaTB.Font = new Font("Leelawadee UI", 12);
+
+
             distancia = distancia + 30;
             return distancia;
         }
@@ -200,13 +246,16 @@ namespace presentationLayer
             grupoSanguineoTB.Location = new Point(480, distancia - 4);
             documentacion.Location = new Point(550, distancia);
             distancia = distancia + 30;
+
             enfermedades.Location = new Point(60, distancia);
             enfermedadesTB.Location = new Point(220, distancia - 4);
             documentacionLB.Location = new Point(550, distancia);
             distancia = distancia + 30;
+
             alergias.Location = new Point(60, distancia);
             alergiasTB.Location = new Point(220, distancia - 4);
             distancia = distancia + 30;
+
             telefono.Location = new Point(60, distancia);
             telefonoTB.Location = new Point(220, distancia - 4);
 
@@ -217,6 +266,19 @@ namespace presentationLayer
             alergiasTB.Size = new Size(120, 30);
             telefonoTB.Size = new Size(60, 30);
             documentacionLB.Size = new Size(190, 95);
+
+            servicio.Font = new Font("Leelawadee UI", 12);
+            servicioTB.Font = new Font("Leelawadee UI", 12);
+            grupoSanguineo.Font = new Font("Leelawadee UI", 12);
+            grupoSanguineoTB.Font = new Font("Leelawadee UI", 12);
+            documentacion.Font = new Font("Leelawadee UI", 12);
+            enfermedades.Font = new Font("Leelawadee UI", 12);
+            enfermedadesTB.Font = new Font("Leelawadee UI", 12);
+            documentacionLB.Font = new Font("Leelawadee UI", 12);
+            alergias.Font = new Font("Leelawadee UI", 12);
+            alergiasTB.Font = new Font("Leelawadee UI", 12);
+            telefono.Font = new Font("Leelawadee UI", 12);
+            telefonoTB.Font = new Font("Leelawadee UI", 12);
 
             distancia = distancia + 120;
 
