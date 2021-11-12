d1957fa9195dce93d994c871f4e6d714d882b87f
hxesx <juan.hueso@uabc.edu.mx>
2021-11-11 20:24:14 -0800

    Actualización 1

---
+++ businessLayer/Gabriel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace businessLayer
+{
+    public class Gabriel
+    {
+        public static List<_1dataLayer.alumnoDTO> alumnosGet()
+        {
+            try
+            {
+                using (_1dataLayer.BDCAMEntities db = new _1dataLayer.BDCAMEntities())
+                {
+                    var query = (from a in db.alumno
+                                 select new _1dataLayer.alumnoDTO
+                                 {
+                                     id_alumno = a.id_alumno,
+
+                                 }).ToList();
+                    return query;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+
+        }
+    }
+}
