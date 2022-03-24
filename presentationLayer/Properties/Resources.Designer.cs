c0e419a3acde5633751c71bc8427658664295cc6
ClaudiaArias01 <arias.claudia8@uabc.edu.mx>
2022-03-23 19:40:54 -0700

    Se subo recurso

--- presentationLayer/Properties/Resources.Designer.cs
+++ presentationLayer/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace presentationLayer.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap add {
+            get {
+                object obj = ResourceManager.GetObject("add", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap agregar {
             get {
                 object obj = ResourceManager.GetObject("agregar", resourceCulture);
