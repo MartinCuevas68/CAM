86231f8cf5dbe12014cf6d7e8ceab52ae57aca24
fernando lugo <fernando.lugo@uabc.edu.mx>
2022-03-29 22:55:26 -0700

    primer comit

--- 1dataLayer/Funciones/Alumnos/DLModificacionAlumno.cs
+++ 1dataLayer/Funciones/Alumnos/DLModificacionAlumno.cs
@@ -8,12 +8,48 @@ namespace _1dataLayer
 {
     public class DLModificacionAlumno
     {
-        public static void Modificacionalumno(int id,alumnoDTO alumno)
+       
+        public static void Modificacionalumno(int id, alumnoDTO alumno)
         {
             using (BDCAMEntities db = new BDCAMEntities())
             {
-                db.SP_ModificacionAlumno(id, alumno.ciclo_escolar, alumno.nombre, alumno.apellido_paterno, alumno.apellido_materno, alumno.fecha_nacimiento, alumno.edad_alumno, alumno.CURP_alumno, alumno.estado_nacimiento_alumno, alumno.ciudad_nacimiento_alumno, alumno.colonia_alumno, alumno.calle_alumno, alumno.numero_alumno, alumno.telefono_personal_alumno, alumno.escuela_procedencia_alumno, alumno.documentacion_alumno, alumno.atendido_por,alumno.tipo_ingreso);
+                db.SP_ModificacionAlumno(id, alumno.ciclo_escolar, alumno.nombre, alumno.apellido_paterno, alumno.apellido_materno, alumno.fecha_nacimiento, alumno.edad_alumno, alumno.CURP_alumno, alumno.estado_nacimiento_alumno, alumno.ciudad_nacimiento_alumno, alumno.colonia_alumno, alumno.calle_alumno, alumno.numero_alumno, alumno.telefono_personal_alumno, alumno.escuela_procedencia_alumno, alumno.documentacion_alumno, alumno.atendido_por, alumno.tipo_ingreso);
             }
         }
+
+        public static void ModificacionEnfermedad(int id, String en)
+        {
+            using (BDCAMEntities db = new BDCAMEntities())
+            {
+                db.Modificar_enfermedades(id, en);
+            }
+        }
+
+        public static void ModificacionDiscapacidad(int id, string disc)
+        {
+            using (BDCAMEntities db = new BDCAMEntities())
+            {
+                db.Modificar_discapacidades(id, disc);
+            }
+        }
+
+        public static void ModificacionAlergia(int id, string alergia)
+        {
+            using (BDCAMEntities db = new BDCAMEntities())
+            {
+                db.SP_Modificar_alergias(id, alergia);
+            }
+        }
+
+        public static void ModificacionTutor(int id,SP_FichaTecnicaAlumnoTutor_Result tutor)
+        {
+           using (BDCAMEntities db = new BDCAMEntities())
+            {
+                Console.WriteLine("Desde data layer: ", tutor.nombre + " ID: "+id);
+                db.SP_ModificacionTutor(id, tutor.nombre, tutor.apellido_paterno, tutor.apellido_materno, tutor.calle_tutor, tutor.colonia_tutor, tutor.numero_tutor, tutor.ocupacion_tutor);
+            }
+        }
+
+
     }
 }
--- businessLayer/Funciones/Alumnos/BLModificacionAlumno.cs
+++ businessLayer/Funciones/Alumnos/BLModificacionAlumno.cs
@@ -8,35 +8,30 @@ namespace businessLayer
 {
     class BLModificacionAlumno
     {
-        /*//Modificacion
-        public static void ModColaborador(int id, string nombre, string telefono, string ncuenta)
+        public static void Main(String[] args)
         {
+            Console.WriteLine("Ingrese la enfermedad nueva: ");
+            String enf = Console.ReadLine();
+            Console.WriteLine("Ingrese la discapacidad nueva: ");
+            String disc = Console.ReadLine();
+            Console.WriteLine("Ingrese la alergia nueva: ");
+            String alerg = Console.ReadLine();
+            Console.WriteLine("Ingrese el id del tutor: ");
+            int id = int.Parse(Console.ReadLine());
+            _1dataLayer.SP_FichaTecnicaAlumnoTutor_Result tutor = new _1dataLayer.SP_FichaTecnicaAlumnoTutor_Result(); 
+            tutor = _1dataLayer.DLConsultaAlumno.FichaTecnicaTutor(id);
+            Console.WriteLine("Tutor escogido: " + tutor.nombre+" "+tutor.apellido_paterno + " ID: "+tutor.id_tutor);
+            Console.WriteLine("Ingrese el nombre del tutor nuevo: ");
+            string nombre = Console.ReadLine();
+            Console.ReadKey();
+            enf.Trim();
+            disc.Trim();
+            alerg.Trim();
+            _1dataLayer.DLModificacionAlumno.ModificacionTutor(tutor.id_tutor, tutor);
+            tutor = _1dataLayer.DLConsultaAlumno.FichaTecnicaTutor(id);
+            Console.WriteLine("Nombre del tutor nuevo: " + tutor.nombre);
+            Console.ReadKey();
 
-            try
-            {
-
-                using (Data_Tutuli.DB_tutuliEntities db = new Data_Tutuli.DB_tutuliEntities())
-                {
-
-
-                    var query = db.colaboradores.SingleOrDefault(b => b.numeroColaborador == id);
-                    if (query != null)
-                    {
-                        query.nombre = nombre;
-                        query.telefono = telefono;
-                        query.cuenta = ncuenta;
-
-                        db.SaveChanges();
-                    }
-
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
-
-        }*/
+        }
     }
 }
