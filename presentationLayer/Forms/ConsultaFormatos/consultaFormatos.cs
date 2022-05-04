30c3f722ec9c4f816cfe0e4bd6e276cbc87fd9f8
fernando lugo <fernando.lugo@uabc.edu.mx>
2022-05-03 21:09:29 -0700

    cambio

--- presentationLayer/Forms/ConsultaFormatos/consultaFormatos.cs
+++ presentationLayer/Forms/ConsultaFormatos/consultaFormatos.cs
@@ -55,5 +55,67 @@ namespace presentationLayer.Forms.ConsultaFormatos
         private void FCI1Pane9_MouseEnter(object sender, EventArgs e) { PLConsultaFormatos.Panel_MouseEnter(FCI9Label); }
         private void FCI1Pane9_MouseLeave(object sender, EventArgs e) { PLConsultaFormatos.Panel_MouseLeave(FCI9Label); }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI1.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI2.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI3.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI4.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI5.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI6.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI7.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI8.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\Formatos\\FCI9.pdf", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+            PDF.src = FileName;
+        }
     }
 }
